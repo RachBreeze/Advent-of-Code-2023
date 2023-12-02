3ef63da9bdae9c0fab6e215ad38029636503e873
Rachel Breeze <hello@rachelbreeze.dev>
2023-12-02 14:46:22 +0000

    Committing code that doesn't give the right answer in case I need it in part 2

---
+++ Day 2/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace Day2
+{
+    internal class Game
+    {
+        public int GameID { get; set; }
+        public int BlueBalls { get; set; }
+        public int GreenBalls { get; set; }
+        public int RedBalls
+        {
+            get; set;
+        }
+    }
+}
---
+++ Day 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using Day2;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = GetGames();
+            var tooMany = games.Where(x => x.RedBalls > 12 || x.GreenBalls > 13 || x.BlueBalls > 14).Select(x => x.GameID).ToList();
+            var validGames = games.Where(x => !tooMany.Contains(x.GameID));
+            var total = validGames.Sum(x => x.GameID);
+            Console.WriteLine(total);
+        }
+        static List<Game> GetGames()
+        {
+
+            var lines = File.ReadAllLines("./../../../input.txt");
+            var games = new List<Game>();
+            for (var i = 0; i < lines.Length; i += 1)
+            {
+                games.Add(ReadGame(lines[i]));
+            }
+            return games;
+        }
+
+        private static Game ReadGame(string line)
+        {
+            line = line.ToLower();
+            var gameContents = line.Split(":");
+            var gameID = gameContents[0].Replace("game ", "");
+            var game = new Game { GameID = int.Parse(gameID) };
+            var hands = gameContents[1].Split(";");
+            foreach (var hand in hands)
+            {
+                var cubes = hand.Split(",");
+                foreach (var cube in cubes)
+                {
+                    var pair = cube.Trim().Split(" ");
+                    var number = int.Parse(pair[0]);
+                    switch (pair[1])
+                    {
+                        case "green":
+                            game.GreenBalls += number;
+                            break;
+                        case "blue":
+                            game.BlueBalls += number;
+                            break;
+                        case "red":
+                            game.RedBalls += number;
+                            break;
+                        default:
+                            throw new Exception("Unrecognised Colour" + pair[1]);
+                    }
+                }
+            }
+            return game;
+        }
+    }
+}
