4d286502723394e4b3983f8b77a41428972a4f4b
Rachel Breeze <hello@rachelbreeze.dev>
2023-12-02 12:26:03 +0000

    Day1Part 1

---
+++ Day 1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("./../../../input.txt");
+            var total = 0;
+            for (var i = 0; i < lines.Length; i += 1)
+            {
+                var line = lines[i];
+                total += GetDigit(line);
+            }
+
+            Console.WriteLine(total);
+        }
+
+        private static int GetDigit(string line)
+        {
+            var letters = line.ToCharArray();
+            var firstDigit = "";
+            var lastDigit = "";
+
+            foreach (var letter in letters)
+            {
+                if (int.TryParse(letter.ToString(), out var result))
+                {
+                    if (string.IsNullOrEmpty(firstDigit))
+                    {
+                        firstDigit = result.ToString();
+                    }
+                    else
+                    {
+                        lastDigit = result.ToString();
+                    }
+                }
+            }
+            if (string.IsNullOrEmpty(lastDigit))
+            {
+                return int.Parse(firstDigit + firstDigit);
+            }
+            return int.Parse(firstDigit + lastDigit);
+        }
+    }
+}
